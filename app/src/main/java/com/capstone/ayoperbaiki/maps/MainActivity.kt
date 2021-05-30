98f419f2ab1147f114dc1db7712daa2510b415e6
irfanripat <irfanripat@gmail.com>
2021-05-30 09:55:34 +0800

    add asset disaster icon

--- app/src/main/java/com/capstone/ayoperbaiki/maps/MainActivity.kt
+++ app/src/main/java/com/capstone/ayoperbaiki/maps/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.capstone.ayoperbaiki
+package com.capstone.ayoperbaiki.maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.ayoperbaiki.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map)
+
     }
 }
